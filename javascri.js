fb8e73c1d3c85049aea51eed40035c51b487a905
amnetdt <amnetdt@yahoo.com>
2020-03-13 11:05:03 +0100

    Just testing

---
+++ javascri.js
@@ -0,0 +1,36 @@
+/*
+var first = prompt("User's First name");
+var last = prompt("User's Last name");
+var age = prompt("User's Age ");
+var fullname = first + " " + last;
+alert("Your fullname is " + first + " " + last);
+alert("Your age is " + age);
+totalday = age * 366;
+console.log("Nice to meet you " + fullname + " " + age);
+console.log("Wow! You have spent " + totalday + " days in life");
+*/
+
+/*
+let a = prompt("What is your age");
+let c = Number(a);
+
+while(c < 6){
+alert("The total days you have spent on earth is " + c);
+c++;
+}
+let age = prompt("Enter your age ");
+if(age >= 18){alert("You are an adult")}else if("age <= 17"){console.log("You are not allow to drink");}
+*/
+var a = 1900;
+
+while(a < 2000){
+console.log(a + 1);
+a++;
+}
+/*
+for(a = 1900; a < 2000; a++){
+    console.log(a);
+}*/
+
+
+
